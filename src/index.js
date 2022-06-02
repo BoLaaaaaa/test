88e86f19e9ff51673abb2ec95152a3ea1eefdd2c
BoLaaaaaa <106610540+BoLaaaaaa@users.noreply.github.com>
2022-06-02 12:25:04 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css"; 
+import  '../src/css/style.css';
+import "bootstrap/dist/js/bootstrap.min.js";
+import "popper.js/dist/popper.min";
+import "jquery/dist/jquery.min";
+
+console.log("اهلا بكم فى متجر عربى");
